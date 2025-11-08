bc455410a1ac7a3ab2b7cc0a81f87a5cfb0ad77b
nicole <nicole@midwestdrifting.com>
2025-11-07 22:44:43 -0500

    actually no, chmod it

--- bin/NR_XML2GFX.sh
+++ bin/NR_XML2GFX.sh
@@ -3,7 +3,8 @@ set -x
 FFDEC_PATH=$(pwd)/ffdec/ffdec
 SOURCE_PATH=$(pwd)/src
 
+chmod +x $FFDEC_PATH
 for file in $SOURCE_PATH/*.xml;
 do
-  bash $FFDEC_PATH -xml2swf "$file" "${file%.xml}.gfx"
+  $FFDEC_PATH -xml2swf "$file" "${file%.xml}.gfx"
 done
